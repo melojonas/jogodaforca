071036cf153e886f66f34d5ebc1d735e5b555750
Gersonsts <118835131+Gersonsts@users.noreply.github.com>
2022-12-04 18:42:30 -0300

    Add files via upload
    lista de palavras e código de escolha randômica

---
+++ palavras.py
@@ -0,0 +1,36 @@
+from random import randint
+def grupo_palavras(modalidade):
+    
+    '''fáceis''' 
+
+    fruta_f = ['Amora' , 'Cereja' , 'Banana' , 'Morango' , 'Laranja' ] 
+    objeto_f = ['Bandeira' , 'Chave' , 'Chinelo' , 'Janela' , 'Mala' ]
+    pais_f = ['Argentina' , 'México' , 'Canadá' , 'Rússia' , 'Sérvia' ]
+    cor_f = ['Azul' , 'Salmão' , 'Rosa' , 'Bege' , 'Lilás' ]
+    animal_f = ['Abelha' , 'Foca' , 'Hiena' , 'Cachorro' , 'Gato' ]
+    objeto_f = ['Bandeira' , 'Chave' , 'Chinelo' , 'Janela' , 'Mala' ]
+
+    
+    sortf = [fruta_f,pais_f,animal_f,objeto_f,cor_f] #Armazena listas de grupo de palavras
+    sort_grupof = sortf[randint(0, len(sortf) -1)] #escolhe aleátorio um grupo de palavras
+
+
+    '''dificeis '''
+   
+    objeto_d = ['Maçaneta' , 'Parafuso' , 'Abajur' , 'Envelope' , 'Espelho' ]
+    animal_d = ['Flamingo' , 'Guiaxinim' , 'Golfinho' , 'Hipopótamo' , 'Garça' ]
+    fruta_d = ['Damasco' , 'carambola' , 'Caqui' , 'Framboesa', 'Tangerina' ]
+    cor_d = ['Turquesa' , 'Sépia' , 'Carmesim' , 'Fúchsia' , 'Índigo' ]
+    pais_d = [ 'Afeganistão' , 'Venezuela' , 'Camarões' , 'Dinamarca' , 'Hungria' ]
+
+
+  
+    sortd = [fruta_d,pais_d,animal_d,objeto_d,cor_d] #Armazena listas de grupo de palavras
+    sort_grupod = sortd[randint(0, len(sortd) -1)] #escolhe aleátorio um grupo de palavras
+
+
+    
+    if modalidade == 2: 
+        return sort_grupod[randint(0, len(sort_grupod) -1)] #escolhe aleatoriamente difícil
+    return  sort_grupof[randint(0, len(sort_grupof) -1)] #escolhe aleatoriamente fácil
+        
