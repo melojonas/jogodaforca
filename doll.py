7f92e03c4f98410ff3953a5375040cb0587249ac
Jonas Melo <114510962+melojonas@users.noreply.github.com>
2022-12-05 09:37:39 -0300

    updates

--- doll.py
+++ doll.py
@@ -1,108 +1,168 @@
-import turtle as t
 from turtle import *
 
-setup(500, 500)
-t.bgcolor('black')
-t.width(4)
-color('white')
-hideturtle()
+setup(1080, 720)
+screensize(1080,720)
+bgcolor('black')
+
+doll = Turtle()
+doll.speed(7)
+doll.width(4)
+doll.color('gray','gray')
+doll.hideturtle()
 
 #cabeça if = 1
-t.circle (50)
+doll.begin_fill()
+doll.circle(50)
+doll.end_fill()
+
 
 #tronco if=2
 
-t.left(270)
-t.forward(130)
-t.penup()
+doll.left(270)
+doll.forward(150)
 
-t.goto(0,0)
-t,left(-270)
+doll.penup()
+doll.home()
 
-t.pendown()
 #Braço esquerdo if=3
-t.left(270)
-t.forward(15)
-t.left(-270)
-t.left(225)
-t.forward(75)
-t.penup()
-t.backward(75)
-t.left(-225)
+doll.goto(0,-25)
+doll.setheading(0)
+doll.pendown()
+
+for x in range(45):
+    doll.seth(x)
+    doll.forward(2)
+
+doll.penup()
+doll.home()
 
 #braço direito if = 4
-t.pendown()
-t.left(315)
-t.forward(75)
-t.backward(75)
-t.left(-315)
+doll.goto(0,-25)
+doll.setheading(180)
+doll.pendown()
 
-t.left(270)
-t.forward(130)
-t.left(-270)
+for x in range(180,135,-1):
+    doll.seth(x)
+    doll.forward(2)
+
+doll.penup()
+doll.home()
 
 #perna esquerda if = 5
-t.left(225)
-t.forward(75)
-t.penup()
-t.backward(75)
-t.left(-225)
+doll.goto(0,-150)
+doll.setheading(315)
+doll.pendown()
 
+doll.forward(75)
+
+doll.penup()
+doll.home()
 
 #perna direita  if = 6
-t.pendown()
-t.left(315)
-t.forward(75)
-t.backward(75)
-t.left(-315)
+doll.goto(0,-150)
+doll.setheading(225)
+doll.pendown()
 
-t.left (90)
-t.penup()
-t.forward(200)
-t.left(-90)
+doll.forward(75)
 
+doll.penup()
+doll.home()
 
 
 #perdeu 6
+doll.clear()
 
-t.forward(25)
-t.left(45)
-t.pendown()
-color('red')
+perdeu = Turtle()
+perdeu.width(4)
+perdeu.speed(-1)
+perdeu.hideturtle()
+perdeu.color('gray','gray')
 
-t.forward(5)
-t.left(-45)
-t.left(225)
-t.forward(10)
-t.penup()
-t.backward(5)
-t.left(-225)
-t.left(125)
-t.pendown()
-t.forward(5)
-t.left(-125)
-t.left(315)
-t.forward(10)
+#cabeça if = 1
+perdeu.begin_fill()
+perdeu.circle(50)
+perdeu.end_fill()
 
-t.penup()
 
-t.left(-315)
-t.backward(50)
-t.pendown()
+#tronco if=2
 
-t.left(45)
-t.pendown()
+perdeu.left(270)
+perdeu.forward(150)
 
-t.forward(5)
-t.left(-45)
-t.left(225)
-t.forward(10)
-t.penup()
-t.backward(5)
-t.left(-225)
-t.left(125)
-t.pendown()
-t.forward(5)
-t.left(-125)
-t.left(315)
-t.forward(10)
+perdeu.penup()
+perdeu.home()
+
+#Braço esquerdo if=3
+perdeu.goto(0,-25)
+perdeu.setheading(0)
+perdeu.pendown()
+
+for x in range(45):
+    perdeu.seth(x)
+    perdeu.forward(2)
+
+perdeu.penup()
+perdeu.home()
+
+#braço direito if = 4
+perdeu.goto(0,-25)
+perdeu.setheading(180)
+perdeu.pendown()
+
+for x in range(180,135,-1):
+    perdeu.seth(x)
+    perdeu.forward(2)
+
+perdeu.penup()
+perdeu.home()
+
+#perna esquerda if = 5
+perdeu.goto(0,-150)
+perdeu.setheading(315)
+perdeu.pendown()
+
+perdeu.forward(75)
+
+perdeu.penup()
+perdeu.home()
+
+#perna direita  if = 6
+perdeu.goto(0,-150)
+perdeu.setheading(225)
+perdeu.pendown()
+
+perdeu.forward(75)
+
+perdeu.penup()
+perdeu.home()
+
+#olhos
+perdeu.color('red')
+perdeu.goto(15,70)
+perdeu.setheading(315)
+perdeu.pendown()
+
+perdeu.forward(15)
+
+perdeu.penup()
+perdeu.goto(25,70)
+perdeu.setheading(225)
+perdeu.pendown()
+
+perdeu.forward(15)
+
+perdeu.penup()
+perdeu.goto(-15,70)
+perdeu.setheading(225)
+perdeu.pendown()
+
+perdeu.forward(15)
+
+perdeu.penup()
+perdeu.goto(-25,70)
+perdeu.setheading(315)
+perdeu.pendown()
+
+perdeu.forward(15)
+
+exitonclick()
